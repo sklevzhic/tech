4cc9bc2e3f6d86462df60f4b0a0ceb913e3b08d6
Alexander <sklevzhic@gmail.com>
2021-08-12 23:57:56 +0300

    fix

--- src/components/ListTechnics/ListTechnics.jsx
+++ src/components/ListTechnics/ListTechnics.jsx
@@ -31,16 +31,41 @@ const ListTechnics = ({technicsByCategory, toogleLoadingInfoFotType, categories}
     //     }).filter(n => n).join(' || ')
     //     return cond
     // }
-    const checkCondition = (el) => {
-        let cond = Object.keys(categories).map(key => {
-            debugger
-        })
-        return cond
-
+    const checkCondition = (arr) => {
+        return arr.some(el => el.year === '2005')
     }
-    console.log(checkCondition('2009'))
 
-    const filteredTechnics = technicsByCategory.map(el => {
+    const filteredTechnics = technicsByCategory.map(obj => {
+        if (checkCondition(obj.properties)) {
+            return <div style={{border: "1px solid"}}>
+                {obj.room}
+                <div>
+                    {
+                        obj.properties.map(el => {
+                            return <div>{el.year} {el.name}</div>
+                        })
+                    }
+                </div>
+            </div>
+        }
+        // return <div style={{border: "1px solid"}}>
+        //     {obj.room}
+        //     <div>
+        //         {
+        //             obj.properties.map(el => {
+        //                 if (checkCondition(el)) {
+        //                     return <div>da</div>
+        //                 } else {
+        //                     return null
+        //                 }
+        {/*            })*/
+        }
+        {/*        }*/
+        }
+        {/*    </div>*/
+        }
+        {/*</div>*/
+        }
     })
     {/*const filteredTechnics = technicsByCategory.map(el => {*/
     }
@@ -69,9 +94,24 @@ const ListTechnics = ({technicsByCategory, toogleLoadingInfoFotType, categories}
 
     return (
         <>
-            {/*{filteredTechnics}*/}
+            {filteredTechnics}
         </>
     )
 }
 
 export default ListTechnics
+
+// <Card key={el.room} className={classes.roomItem}>
+//     <Typography variant={"h5"} className={`${classes.roomNumber}`}>
+// {!(Number.parseInt(el.room)) ? el.room : `${el.room} кабинет`}
+//
+// </Typography>*/
+//
+// <List dense>
+//     {el.properties.map((el1, i) => {
+//         return !toogleLoadingInfoFotType
+//             ? <MiniCardTechnic key={i} el={el1}/>
+//             : <MiniCardTechnicSkeleton key={i}/>
+//     })}
+// </List>
+// </Card>
